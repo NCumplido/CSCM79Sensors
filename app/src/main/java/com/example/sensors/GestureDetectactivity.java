cc07b159776fdf3ddf8bbd3617bfacc42fbda309
Nachodood <ncumplido@msn.com>
2020-03-13 10:39:48 +0000

    Link GestureDetect

---
+++ app/src/main/java/com/example/sensors/GestureDetectactivity.java
@@ -0,0 +1,16 @@
+package com.example.sensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestureDetectactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gesture_detectactivity);
+
+
+    }
+}
